dd618e8c69dbb0520547c0448520279c7c08f6a7
Anton Maksimov <anton.maximov@gmail.com>
2021-01-19 20:28:16 +0700

    Update admin config

--- models/request.go
+++ models/request.go
@@ -1,12 +1,16 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type Request struct {
-	Id 			int    	`json:"id"`
+	gorm.Model
 	Name 		string 	`json:"first_name"`
 	Surname		string 	`json:"last_name"`
 	Education 	string 	`json:"education"`
+	Email	 	string 	`json:"email"`
 	Why			string 	`json:"reason"`
 	Direction	string  `json:"course"`
+	Link		string  `json:"link"`
 }
 
 func (Request) TableName() string {
--- myAdminConf/myAdmin.go
+++ myAdminConf/myAdmin.go
@@ -6,6 +6,8 @@ import (
 	"handh-school-back/models"
 	"handh-school-back/database"
 	"github.com/qor/admin"
+	"github.com/jinzhu/gorm"
+	"github.com/qor/qor"
 )
 
 func InitAdmin() *http.ServeMux {
@@ -13,9 +15,44 @@ func InitAdmin() *http.ServeMux {
 	DB.AutoMigrate(&models.Request{})
 
 	Admin := admin.New(&admin.AdminConfig{DB: DB})
-	Admin.AddResource(&models.Request{})
+	reqRes := Admin.AddResource(&models.Request{}, &admin.Config{
+		Name:       "Заявки",
+		Menu:       []string{"Заявки"},
+	})
 
-	&
+	reqRes.Meta(&admin.Meta{Name: "Name", Valuer: func(record interface{}, context *qor.Context) interface{} {
+		if p, ok := record.(*models.Request); ok {
+			return p.Name + " " + p.Surname
+		}
+		return ""
+	}})
+	reqRes.Meta(&admin.Meta{Name: "Name", Label: "Имя"})
+	reqRes.Meta(&admin.Meta{Name: "Surname", Label: "Фамилия"})
+	reqRes.Meta(&admin.Meta{Name: "Education", Label: "Образование"})
+	reqRes.Meta(&admin.Meta{Name: "Email", Label: "Почта"})
+	reqRes.Meta(&admin.Meta{Name: "Why", Label: "Причина поступления"})
+	reqRes.Meta(&admin.Meta{Name: "Link", Label: "Тестовое"})
+	reqRes.Meta(&admin.Meta{
+		Name: "Direction",
+		Label: "Направление",
+		Config: &admin.SelectOneConfig{Collection: []string{
+			"iOS", "Android", "Frontend", "Backend (Java)",
+		}}})
+
+	reqRes.IndexAttrs("-Id", "-Surname", "-Education", "-Why", "-Link")
+
+	reqRes.Scope(&admin.Scope{Name: "iOS", Group:"Направление", Handler: func(db *gorm.DB, context *qor.Context) *gorm.DB {
+		return db.Where("Direction like ?", "iOS")
+	}})
+	reqRes.Scope(&admin.Scope{Name: "Android", Group:"Направление", Handler: func(db *gorm.DB, context *qor.Context) *gorm.DB {
+		return db.Where("Direction like ?", "Android")
+	}})
+	reqRes.Scope(&admin.Scope{Name: "Frontend", Group:"Направление", Handler: func(db *gorm.DB, context *qor.Context) *gorm.DB {
+		return db.Where("Direction like ?", "Frontend")
+	}})
+	reqRes.Scope(&admin.Scope{Name: "Backend (Java)", Group:"Направление", Handler: func(db *gorm.DB, context *qor.Context) *gorm.DB {
+		return db.Where("Direction like ?", "Backend (Java)")
+	}})
 
 	m := http.NewServeMux()
 	Admin.MountTo("/admin", m)
